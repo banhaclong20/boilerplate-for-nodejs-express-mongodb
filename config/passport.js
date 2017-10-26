e640b44bb7e43b5be363cebec8d2530fb16b57a5
Tom Tran <ttran@xpressdocs.com>
2017-10-26 12:14:29 -0500

    Initial Commit

--- config/passport.js
+++ config/passport.js
@@ -13,9 +13,6 @@ module.exports = function(passport) {
             callbackURL: '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            // console.log(accessToken);
-            // console.log(profile);
-
             const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
 
             const newUser = {
